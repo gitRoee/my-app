c06642c671989752a31e0fdf35504b3fa362ab90
GitRoee <roeekl0@gmail.com>
2024-02-02 18:04:17 +0200

    startup

---
+++ app/index.tsx
@@ -0,0 +1,17 @@
+import { router } from "expo-router";
+import { View, Text, Pressable } from "react-native"
+
+const index = () => {
+
+    return (
+        <View>
+            {   
+                // <Pressable onPress={() => router.push("/pages/1")}>
+                //<Text>Home</Text>
+                //</Pressable>
+            }
+        </View >
+    )
+};
+
+export default index;
